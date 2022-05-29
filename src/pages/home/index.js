4d42a4bfec7ccb1a66add9af02b1182b2de466a4
Jeferson Queiroga <jefersonqueiroga@gmail.com>
2022-05-29 15:19:52 -0300

    Adicionar projeto git

---
+++ src/pages/home/index.js
@@ -0,0 +1,49 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import {Wrapper,
+  Profile,
+  Adress,
+  Lable,
+  AdressValue,
+  Img,
+  Location,
+  Store,
+  Hour,
+  Delivery,
+  ItemHour,
+  LabelHour,
+  LableStore,
+  StatusDelivery,
+  LabelStatus
+
+} from './styles'
+import { Ionicons } from '@expo/vector-icons'; 
+
+import ImageProfile from '../../images/profile.png';
+import Logotipo from '../../images/logotipo.png';
+
+import PainelHour from '../../components/PanelHour';
+import Input from '../../components/Input';
+export default function Home() {
+  return (
+    <Wrapper>
+      <Profile>
+        <Location>
+          <Ionicons name="location" size={35} color="#F9B233" />
+          <Adress>
+            <Lable>Localização</Lable>
+            <AdressValue>Rua Francisco Sales</AdressValue>
+          </Adress>
+        </Location>
+        <Img source={ImageProfile}/>
+      </Profile>
+      <Store>
+        <Img source={Logotipo}/>
+        <LableStore>Restaurante e Pizzaria Divino Sabor</LableStore>
+      </Store>
+
+      <PainelHour/>
+      <Input/>
+    </Wrapper>
+  );
+}
